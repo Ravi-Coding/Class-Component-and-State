e600e8a6644696611ff5fdf61735d8b1f8a85b0c
Ravindra <rk2671353@gmail.com>
2023-07-28 11:28:05 +0000

    CoDe

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,3 +1,9 @@
-const Welcome = () => <h1>Hello, User</h1>
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    return <h1>Hello, User</h1>
+  }
+}
 
 export default Welcome
